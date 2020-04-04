5470de76352ffe23c3a2d5c3d9095636052141f0
stg24 <63126156+stg24@users.noreply.github.com>
2020-04-04 17:33:44 +0200

    v3

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, g
 from flask import render_template
+import sqlite3
+
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
+
+def open_connection():
+    connection = getattr(g._connection, None)
+    if connection is None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def execute_sql(sql, values = (), commit = False, single = False):
+    connection = open_connection()
 
 @app.route('/')
 @app.route('/jobs')
