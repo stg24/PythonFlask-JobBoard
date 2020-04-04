6a2472eb788c7c6aaf3b21ccb9c56be830b5f6f9
stg24 <63126156+stg24@users.noreply.github.com>
2020-04-04 17:39:48 +0200

    v3a

--- jobs/app.py
+++ jobs/app.py
@@ -7,7 +7,7 @@ PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
 
 def open_connection():
-    connection = getattr(g._connection, None)
+    connection = getattr(g, '_connection', None)
     if connection is None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
